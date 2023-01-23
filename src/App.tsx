273c31ace83d6a93068a67631e1d02d700f6446f
gabriel <gos.gabri@gmail.com>
2023-01-23 17:09:57 -0400

    Instalação do pacote phosphor-react, criação do componente Header e SummaryTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './styles/global.css';
 
-import { Habits } from "./components/Habits"
+import { Header } from './components/Header';
+import { SummerTable } from './components/SummaryTable';
 
-function App() {
-  return <Habits completed={3} />
+export function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <SummerTable />
+      </div>
+    </div>
+  )
 }
-
-export default App
---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,5 @@
+export function SummerTable () {
+    return (
+        <div></div>
+    )
+}
